d27b09c7b2e41a3d6bc28a014441d09c2cfbe79a
carlstep <>
2021-05-09 20:54:35 +0000

    added multiline string

--- main.dart
+++ main.dart
@@ -2,4 +2,14 @@ void main() {
 	String dartLang = "Dart";
     String javaLang = "Java";
     print("Saying that $dartLang is identical to $javaLang might be a bit too much... At least when we count the letters in their names we get ${dartLang.length} in both cases.");
+
+    String multiLine = '''
+    this
+    is
+    a
+    multiline
+    string
+    ''';
+        print("$multiLine");
 }
+// expressions are embedded into strings using ${expression_name}. If the expression is an identifier, then omit the {}.
