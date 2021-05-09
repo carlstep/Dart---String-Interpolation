b5638cd18a5c219812826dd99bfb00f99d695542
carlstep <>
2021-05-09 20:37:51 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,5 @@
+void main() {
+	String dartLang = "Dart";
+    String javaLang = "Java";
+    print("Saying that $dartLang is identical to $javaLang might be a bit too much... At least when we count the letters in their names we get ${dartLang.length} in both cases.");
+}
